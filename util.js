301884253769163a0b3dcc9d8367103e6ede1601
smh28 <sandrahuldudottir@gmail.com>
2017-11-19 22:32:33 +0000

    writeText method in utils

--- util.js
+++ util.js
@@ -103,6 +103,14 @@ fillBox: function (ctx, x, y, w, h, style) {
     ctx.fillStyle = oldStyle;
 },
 
+writeText: function(ctx, x, y, style, text, value) {
+    ctx.font = "Bold 20px Arial";
+    ctx.fillStyle = style;
+    ctx.beginPath();
+    //ctx.textAlign = "center";
+    ctx.fillText(text + " " + value, x, y);
+    ctx.closePath();
+},
 
 //Draw a line by giving the coordinates for the beginning point and ending point of the line,
 //along with the desired line width and style.
